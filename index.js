3a857784aa19063765306e290ed49163fed02867
jzolo22 <jzolotarev@gmail.com>
2020-12-05 13:58:15 -0500

    commit tree 268db3e90b922fcc38d9ec600bb5a6a019c829fa

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+  let win = array.find(record => record.result === "W")
+  if (win) {
+    return win.year
+  } else {
+    return undefined
+  } 
+}
